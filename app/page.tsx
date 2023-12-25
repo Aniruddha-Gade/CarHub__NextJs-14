ad484860e02b2c9a248ddb6cce31bbfc44d53204
unknown <gadeaniruddha2@gmail.com>
2023-12-25 11:55:47 +0530

    change: import structure of file

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import React, { useState, useEffect } from 'react';
 import { Hero, SearchBar, CustomFilter, CarCard, ShowMore } from '@/components'
 import { fetchCars } from '@/utils'
 import { fuels, yearsOfProduction } from '@/constants';
-import { HomeProps } from '@types'
+import { HomeProps } from '@/types'
 
 
 // skeleton body
